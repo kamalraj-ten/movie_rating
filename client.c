5a7217a8c047bbda01441dc636d6609fa75e4c25
kamalraj <kamalrajdhanakodi@gmail.com>
2021-08-16 20:10:29 +0530

    initial commit

---
+++ client.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <string.h>
+
+#define PORT 8888
+#define NAMESIZE 20
+
+int main()
+{
+    int socket_fd = socket(PF_INET, SOCK_STREAM, 0);
+
+    if (socket_fd <= 0) {
+        printf("Error in creating socket\n");
+        return -1;
+    }
+
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_family = PF_INET;
+
+    if(inet_pton(PF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if ( connect(socket_fd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
+        printf("Connection error\n");
+        return -1;
+    }
+
+    char name[NAMESIZE] = "asuran\0";
+    send(socket_fd, name, strlen(name), 0);
+
+    int rating = 4;
+    send(socket_fd, &rating, sizeof(rating), 0);
+
+    printf("Completed sending data\n");
+
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+
+#define PORT 8888
+#define BUFFERSIZE 1024
+#define NAMESIZE 20
+
+struct Movie {
+    char name[NAMESIZE];
+    int rating;
+};
+
+void printMovie(struct Movie m) {
+    printf("name: %s\trating: %d\n", m.name, m.rating);
+}
+
+int main()
+{
+    int server_fd = socket(PF_INET, SOCK_STREAM, 0);
+    if (server_fd <= 0) {
+        printf("Error creating the socket\n");
+        return -1;
+    }
+
+    //forcefully attaching socket to port
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        return -1;
+    }
+
+    // binding the socket
+    struct sockaddr_in address;
+    address.sin_family = PF_INET;
+    address.sin_port = htons(PORT);
+    address.sin_addr.s_addr = INADDR_ANY;
+
+    if ( bind(server_fd, (struct sockaddr*) &address, sizeof(address)) < 0 ) {
+        printf("Error in binding\n");
+        return -1;
+    }
+
+     // listening
+    if (listen(server_fd, 2) < 0)
+    {
+        printf("Error Listening\n");
+        return -1;
+    }
+
+    //accepting other sockets
+    // currently accepting only one
+    int client_fd, addr_len = sizeof(address);
+    if ((client_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addr_len)) < 0)
+    {
+        printf("Error accepting sockets\n");
+        return -1;
+    }
+
+    struct Movie m;
+    // recv movie
+    // name
+    int readval = recv(client_fd, m.name, NAMESIZE, 0);
+    readval = recv(client_fd, &m.rating, sizeof(int), 0);
+    printMovie(m);
+
+    return 0;
+}
